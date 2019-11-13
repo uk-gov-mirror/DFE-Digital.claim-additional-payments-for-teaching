7c6ab8f9331fd83ed3e8e59cbae6f57f17dd885d
Stuart Harrison <pezholio@gmail.com>
2019-11-13 14:50:07 +0000

    Add a job to record whenever a claim is submitted
    This sends a claim ID, the policy name and the date it was submitted to
    Geckoboard. As we're going to send a bunch of different data to Geckoboard
    the job inherits from a GeckoboardJob subclass

---
+++ spec/jobs/record_submitted_claim_job_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+require "geckoboard"
+
+RSpec.describe RecordSubmittedClaimJob do
+  let(:claim) { build(:claim, :submitted) }
+  let(:client) { double("Geckoboard::Client") }
+  let(:dataset) { double("Geckoboard::Dataset") }
+
+  subject { described_class.new }
+
+  before do
+    allow(subject).to receive(:client).and_return(client)
+    allow(client).to receive_message_chain(:datasets, :find_or_create).with(
+      subject.dataset_name,
+      fields: subject.fields
+    ).and_return(dataset)
+  end
+
+  it "sends the correct data to Geckoboard" do
+    expect(dataset).to receive(:post).with([
+      {
+        reference: claim.reference,
+        policy: claim.policy.to_s,
+        submitted_at: claim.submitted_at,
+      },
+    ])
+
+    subject.perform(claim)
+  end
+
+  it "has the right dataset name" do
+    ClimateControl.modify ROLLBAR_ENV: "test" do
+      expect(subject.dataset_name).to eq("claims.submitted.test")
+    end
+  end
+
+  it "has the right fields" do
+    expect(subject.fields.count).to eq(3)
+
+    expect(subject.fields[0]).to be_a(Geckoboard::StringField)
+    expect(subject.fields[0].id).to eq(:reference)
+    expect(subject.fields[0].name).to eq("Reference")
+
+    expect(subject.fields[1]).to be_a(Geckoboard::StringField)
+    expect(subject.fields[1].id).to eq(:policy)
+    expect(subject.fields[1].name).to eq("Policy")
+
+    expect(subject.fields[2]).to be_a(Geckoboard::DateTimeField)
+    expect(subject.fields[2].id).to eq(:submitted_at)
+    expect(subject.fields[2].name).to eq("Date Submitted")
+  end
+end
