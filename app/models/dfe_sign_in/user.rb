81ded56eae8d00c110b3a5468c29b64f00f765dd
Stuart Harrison <pezholio@gmail.com>
2019-12-11 16:48:40 +0000

    Add full_name method to user model

--- app/models/dfe_sign_in/user.rb
+++ app/models/dfe_sign_in/user.rb
@@ -7,4 +7,8 @@ class DfeSignIn::User < ApplicationRecord
   validates :family_name, presence: true
   validates :email, presence: true
   validates :organisation_name, presence: true
+
+  def full_name
+    [given_name, family_name].join(" ")
+  end
 end
--- spec/models/dfe_sign_in/user_spec.rb
+++ spec/models/dfe_sign_in/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe DfeSignIn::User, type: :model do
   it { should validate_presence_of(:family_name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:organisation_name) }
+
+  let(:user) { build(:dfe_signin_user) }
+
+  describe "full_name" do
+    it "returns a full name" do
+      expect(user.full_name).to eq("Jo Bloggs")
+    end
+  end
 end
