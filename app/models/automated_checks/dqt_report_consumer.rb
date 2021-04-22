DFE-Digital/claim-additional-payments-for-teaching

Automatically update admin claim tasks with DQT API

app/models/automated_checks/dqt_report_consumer.rb
@@ -28,8 +28,11 @@ module AutomatedChecks
           claim = claim_for_record(record)
 
           if claim
-            perform_qualification_check(claim, record)
-            perform_identity_confirmation(claim, record)
+            @completed_tasks += UpdateAdminClaimTasksWithDqt.new(
+              claim: claim,
+              admin_user: @admin_user,
+              dqt_teacher_statutes: record
+            ).perform
           end
         end
       end
@@ -45,20 +48,6 @@ module AutomatedChecks
 
     private
 
-    def perform_qualification_check(claim, record)
-      if awaiting_task?(claim, "qualifications") && claim.policy::DqtRecord.new(record).eligible?
-        claim.tasks.create!(task_attributes("qualifications"))
-        @completed_tasks += 1
-      end
-    end
-
-    def perform_identity_confirmation(claim, record)
-      if claim.identity_verified? && awaiting_task?(claim, "identity_confirmation") && identity_matches?(claim, record)
-        claim.tasks.create!(task_attributes("identity_confirmation"))
-        @completed_tasks += 1
-      end
-    end
-
     def claims
       @claims ||= Claim.awaiting_decision.includes(:tasks)
     end
@@ -67,25 +56,8 @@ module AutomatedChecks
       claims.detect { |c| c.reference == record.fetch(:claim_reference) }
     end
 
-    def awaiting_task?(claim, task_name)
-      claim.tasks.none? { |task| task.name == task_name }
-    end
-
-    def identity_matches?(claim, record)
-      record.fetch(:date_of_birth) == claim.date_of_birth && record.fetch(:surname)&.casecmp?(claim.surname)
-    end
-
     def dqt_records
       @dqt_records ||= DqtReportCsvToRecords.new(@csv.rows).transform
-    end
-
-    def task_attributes(task_name)
-      {
-        name: task_name,
-        passed: true,
-        manual: false,
-        created_by: @admin_user
-      }
     end
   end
 end

app/models/automated_checks/update_admin_claim_tasks_with_dqt.rb
@@ -1,0 +1,57 @@
+module AutomatedChecks
+  class UpdateAdminClaimTasksWithDqt
+    def initialize(
+      claim:,
+      dqt_teacher_statutes:,
+      admin_user: nil
+    )
+      self.admin_user = admin_user
+      self.claim = claim
+      self.qualified_teaching_status = dqt_teacher_statutes
+
+      @completed_tasks = 0
+    end
+
+    def perform
+      perform_qualification_check
+      perform_identity_confirmation
+
+      @completed_tasks
+    end
+
+    private
+
+    attr_accessor :admin_user, :claim, :qualified_teaching_status
+
+    def awaiting_task?(task_name)
+      claim.tasks.none? { |task| task.name == task_name }
+    end
+
+    def identity_matches?
+      qualified_teaching_status.fetch(:date_of_birth) == claim.date_of_birth && qualified_teaching_status.fetch(:surname)&.casecmp?(claim.surname)
+    end
+
+    def perform_qualification_check
+      if awaiting_task?("qualifications") && claim.policy::DqtRecord.new(qualified_teaching_status).eligible?
+        claim.tasks.create!(task_attributes("qualifications"))
+        @completed_tasks += 1
+      end
+    end
+
+    def perform_identity_confirmation
+      if claim.identity_verified? && awaiting_task?("identity_confirmation") && identity_matches?
+        claim.tasks.create!(task_attributes("identity_confirmation"))
+        @completed_tasks += 1
+      end
+    end
+
+    def task_attributes(task_name)
+      {
+        name: task_name,
+        passed: true,
+        manual: false,
+        created_by: admin_user
+      }
+    end
+  end
+end

uk-gov-mirror/DFE-Digital.claim-additional-payments-for-teaching

app/models/automated_checks/dqt_report_consumer.rb
@@ -28,8 +28,11 @@ module AutomatedChecks
           claim = claim_for_record(record)
 
           if claim
-            perform_qualification_check(claim, record)
-            perform_identity_confirmation(claim, record)
+            @completed_tasks += UpdateAdminClaimTasksWithDqt.new(
+              claim: claim,
+              admin_user: @admin_user,
+              dqt_teacher_status: record
+            ).perform
           end
         end
       end
@@ -45,20 +48,6 @@ module AutomatedChecks
 
     private
 
-    def perform_qualification_check(claim, record)
-      if awaiting_task?(claim, "qualifications") && claim.policy::DqtRecord.new(record).eligible?
-        claim.tasks.create!(task_attributes("qualifications"))
-        @completed_tasks += 1
-      end
-    end
-
-    def perform_identity_confirmation(claim, record)
-      if claim.identity_verified? && awaiting_task?(claim, "identity_confirmation") && identity_matches?(claim, record)
-        claim.tasks.create!(task_attributes("identity_confirmation"))
-        @completed_tasks += 1
-      end
-    end
-
     def claims
       @claims ||= Claim.awaiting_decision.includes(:tasks)
     end
@@ -67,25 +56,8 @@ module AutomatedChecks
       claims.detect { |c| c.reference == record.fetch(:claim_reference) }
     end
 
-    def awaiting_task?(claim, task_name)
-      claim.tasks.none? { |task| task.name == task_name }
-    end
-
-    def identity_matches?(claim, record)
-      record.fetch(:date_of_birth) == claim.date_of_birth && record.fetch(:surname)&.casecmp?(claim.surname)
-    end
-
     def dqt_records
       @dqt_records ||= DqtReportCsvToRecords.new(@csv.rows).transform
-    end
-
-    def task_attributes(task_name)
-      {
-        name: task_name,
-        passed: true,
-        manual: false,
-        created_by: @admin_user
-      }
     end
   end
 end

app/models/automated_checks/update_admin_claim_tasks_with_dqt.rb
@@ -1,0 +1,57 @@
+module AutomatedChecks
+  class UpdateAdminClaimTasksWithDqt
+    def initialize(
+      claim:,
+      dqt_teacher_status:,
+      admin_user: nil
+    )
+      self.admin_user = admin_user
+      self.claim = claim
+      self.qualified_teaching_status = dqt_teacher_status
+
+      @completed_tasks = 0
+    end
+
+    def perform
+      perform_qualification_check
+      perform_identity_confirmation
+
+      @completed_tasks
+    end
+
+    private
+
+    attr_accessor :admin_user, :claim, :qualified_teaching_status
+
+    def awaiting_task?(task_name)
+      claim.tasks.none? { |task| task.name == task_name }
+    end
+
+    def identity_matches?
+      qualified_teaching_status.fetch(:date_of_birth) == claim.date_of_birth && qualified_teaching_status.fetch(:surname)&.casecmp?(claim.surname)
+    end
+
+    def perform_qualification_check
+      if awaiting_task?("qualifications") && claim.policy::DqtRecord.new(qualified_teaching_status).eligible?
+        claim.tasks.create!(task_attributes("qualifications"))
+        @completed_tasks += 1
+      end
+    end
+
+    def perform_identity_confirmation
+      if claim.identity_verified? && awaiting_task?("identity_confirmation") && identity_matches?
+        claim.tasks.create!(task_attributes("identity_confirmation"))
+        @completed_tasks += 1
+      end
+    end
+
+    def task_attributes(task_name)
+      {
+        name: task_name,
+        passed: true,
+        manual: false,
+        created_by: admin_user
+      }
+    end
+  end
+end