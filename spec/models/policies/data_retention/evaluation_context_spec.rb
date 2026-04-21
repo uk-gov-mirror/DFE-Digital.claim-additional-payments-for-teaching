DFE-Digital/claim-additional-payments-for-teaching

Data retention policy refactor
Adds a declarative data retention policy, initialy for TRI.

Currently understanding how long attributes are retained for a given
policy is a bit tricky and requires parsing the scopes in
`Policies::ClaimPersonalDataScrubber` along with tracing back which
attributes are set in `PERSONAL_DATA_ATTRIBUTES_TO_DELETE`,
`PERSONAL_DATA_ATTRIBUTES_TO_RETAIN_FOR_EXTENDED_PERIOD`, and how
`EXTENDED_PERIOD_END_DATE` works.
When the product team ask how long is attribute X retained for policy Y
we want to be able to answer them quickly and confidently.

This commit lays the initial ground work for an easier to understand
declarative data retention definition.

Policies now define a `Policies::<policy>::DataRetention::Policy`
class which specifies which how long each attribute is to be retained
for, in "English".

We have two classes shared by all policies
`ChangeSet` which returns the new attributes for the claim after
redactions, if any, have been applied.

`EvaluationContext` which handles date related queries against the
claim. Any methods used in the `DataRetentionPolicy` attribute
definitions should be defined on the `EvaluationContext`.

Changing data retention policy consits of:
* Defining a new duration on `EvaluationContext` (if a new one is
  required)
* Updating the attributes in `<policy>::DataRetention::Policy` to use the
  duration

We may want to consider versioning data retention policies by academic
year, though that's out of scope of this commit.

spec/models/policies/data_retention/evaluation_context_spec.rb
@@ -1,0 +1,176 @@
+require "rails_helper"
+
+RSpec.describe Policies::DataRetention::EvaluationContext do
+  let(:evaluation_context) { described_class.new(claim) }
+
+  let(:current_academic_year) { AcademicYear.new(2025) }
+
+  let(:previous_academic_year) { current_academic_year.previous }
+
+  let(:in_current_academic_year) do
+    current_academic_year.start_of_autumn_term + 1.second
+  end
+
+  let(:in_previous_academic_year) do
+    previous_academic_year.start_of_autumn_term + 1.second
+  end
+
+  before do
+    allow(AcademicYear).to receive(:current) { current_academic_year }
+  end
+
+  describe "#old_paid_claim?" do
+    subject { evaluation_context.old_paid_claim? }
+
+    let(:claim) { create(:claim) }
+
+    context "when the claim is not paid" do
+      it { is_expected.to be false }
+    end
+
+    context "when the claim is paid" do
+      context "when the claim's scheduled payment date is in the current academic year" do
+        before do
+          create(
+            :payment,
+            claims: [claim],
+            scheduled_payment_date: in_current_academic_year
+          )
+        end
+
+        it { is_expected.to be false }
+      end
+
+      context "when the claim's scheduled payment date is in the previous academic year" do
+        before do
+          create(
+            :payment,
+            claims: [claim],
+            scheduled_payment_date: in_previous_academic_year
+          )
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+
+  describe "#old_rejected_claim?" do
+    subject { evaluation_context.old_rejected_claim? }
+
+    let(:claim) { create(:claim, :submitted) }
+
+    context "when the claim is undecided" do
+      it { is_expected.to be false }
+    end
+
+    context "when the claim is approved" do
+      before do
+        create(
+          :decision,
+          :approved,
+          claim: claim,
+          created_at: in_previous_academic_year
+        )
+      end
+
+      it { is_expected.to be false }
+    end
+
+    context "when the claim is rejected" do
+      context "when the claim was rejected in the current academic year" do
+        before do
+          create(
+            :decision,
+            :rejected,
+            claim: claim,
+            created_at: in_current_academic_year
+          )
+        end
+
+        it { is_expected.to be false }
+      end
+
+      context "when the claim was rejected in a previous academic year" do
+        before do
+          create(
+            :decision,
+            :rejected,
+            claim: claim,
+            created_at: in_previous_academic_year
+          )
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+
+  describe "#submitted_in_previous_academic_term?" do
+    subject { evaluation_context.submitted_in_previous_academic_term? }
+
+    context "when the claim was submitted in the current AY" do
+      let(:claim) { create(:claim, academic_year: current_academic_year) }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the claim was submitted in a prior AY" do
+      let(:claim) { create(:claim, academic_year: previous_academic_year) }
+
+      it { is_expected.to be true }
+    end
+  end
+
+  describe "#inactive_claim_submitted_in_previous_academic_term?" do
+    subject { evaluation_context.inactive_claim_submitted_in_previous_academic_term? }
+
+    context "when the claim was submitted in the current AY" do
+      let(:claim) { create(:claim, academic_year: current_academic_year) }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the claim was submitted in a prior AY" do
+      context "when the claim is active" do
+        context "when the claim is undecided" do
+          let(:claim) { create(:claim, academic_year: previous_academic_year) }
+
+          it { is_expected.to be false }
+        end
+
+        context "when the claim is approved but unpaid" do
+          let(:claim) do
+            create(:claim, :submitted, academic_year: previous_academic_year)
+          end
+
+          before do
+            create(
+              :decision,
+              :approved,
+              claim: claim,
+              created_at: in_previous_academic_year
+            )
+          end
+
+          it { is_expected.to be false }
+        end
+      end
+
+      context "when the claim is inactive" do
+        let(:claim) { create(:claim, academic_year: previous_academic_year) }
+
+        before do
+          create(
+            :decision,
+            :rejected,
+            claim: claim,
+            created_at: in_previous_academic_year
+          )
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+end

uk-gov-mirror/DFE-Digital.claim-additional-payments-for-teaching

spec/models/policies/data_retention/evaluation_context_spec.rb
@@ -1,0 +1,176 @@
+require "rails_helper"
+
+RSpec.describe Policies::DataRetention::EvaluationContext do
+  let(:evaluation_context) { described_class.new(claim) }
+
+  let(:current_academic_year) { AcademicYear.new(2025) }
+
+  let(:previous_academic_year) { current_academic_year.previous }
+
+  let(:in_current_academic_year) do
+    current_academic_year.start_of_autumn_term + 1.second
+  end
+
+  let(:in_previous_academic_year) do
+    previous_academic_year.start_of_autumn_term + 1.second
+  end
+
+  before do
+    allow(AcademicYear).to receive(:current) { current_academic_year }
+  end
+
+  describe "#old_paid_claim?" do
+    subject { evaluation_context.old_paid_claim? }
+
+    let(:claim) { create(:claim) }
+
+    context "when the claim is not paid" do
+      it { is_expected.to be false }
+    end
+
+    context "when the claim is paid" do
+      context "when the claim's scheduled payment date is in the current academic year" do
+        before do
+          create(
+            :payment,
+            claims: [claim],
+            scheduled_payment_date: in_current_academic_year
+          )
+        end
+
+        it { is_expected.to be false }
+      end
+
+      context "when the claim's scheduled payment date is in the previous academic year" do
+        before do
+          create(
+            :payment,
+            claims: [claim],
+            scheduled_payment_date: in_previous_academic_year
+          )
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+
+  describe "#old_rejected_claim?" do
+    subject { evaluation_context.old_rejected_claim? }
+
+    let(:claim) { create(:claim, :submitted) }
+
+    context "when the claim is undecided" do
+      it { is_expected.to be false }
+    end
+
+    context "when the claim is approved" do
+      before do
+        create(
+          :decision,
+          :approved,
+          claim: claim,
+          created_at: in_previous_academic_year
+        )
+      end
+
+      it { is_expected.to be false }
+    end
+
+    context "when the claim is rejected" do
+      context "when the claim was rejected in the current academic year" do
+        before do
+          create(
+            :decision,
+            :rejected,
+            claim: claim,
+            created_at: in_current_academic_year
+          )
+        end
+
+        it { is_expected.to be false }
+      end
+
+      context "when the claim was rejected in a previous academic year" do
+        before do
+          create(
+            :decision,
+            :rejected,
+            claim: claim,
+            created_at: in_previous_academic_year
+          )
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+
+  describe "#submitted_in_previous_academic_term?" do
+    subject { evaluation_context.submitted_in_previous_academic_term? }
+
+    context "when the claim was submitted in the current AY" do
+      let(:claim) { create(:claim, academic_year: current_academic_year) }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the claim was submitted in a prior AY" do
+      let(:claim) { create(:claim, academic_year: previous_academic_year) }
+
+      it { is_expected.to be true }
+    end
+  end
+
+  describe "#inactive_claim_submitted_in_prior_academic_term?" do
+    subject { evaluation_context.inactive_claim_submitted_in_prior_academic_term? }
+
+    context "when the claim was submitted in the current AY" do
+      let(:claim) { create(:claim, academic_year: current_academic_year) }
+
+      it { is_expected.to be false }
+    end
+
+    context "when the claim was submitted in a prior AY" do
+      context "when the claim is active" do
+        context "when the claim is undecided" do
+          let(:claim) { create(:claim, academic_year: previous_academic_year) }
+
+          it { is_expected.to be false }
+        end
+
+        context "when the claim is approved but unpaid" do
+          let(:claim) do
+            create(:claim, :submitted, academic_year: previous_academic_year)
+          end
+
+          before do
+            create(
+              :decision,
+              :approved,
+              claim: claim,
+              created_at: in_previous_academic_year
+            )
+          end
+
+          it { is_expected.to be false }
+        end
+      end
+
+      context "when the claim is inactive" do
+        let(:claim) { create(:claim, academic_year: previous_academic_year) }
+
+        before do
+          create(
+            :decision,
+            :rejected,
+            claim: claim,
+            created_at: in_previous_academic_year
+          )
+        end
+
+        it { is_expected.to be true }
+      end
+    end
+  end
+end